bc91fd3dcb3e52f953372264a661ff0847105e83
Peter Nguyen <nguyenph882@gmail.com>
2018-01-27 20:35:21 -0800

    change trader price

--- DayZ_Epoch_11.Chernarus/dayz_code/configs/Category - Namalsk/sniperRifleAmmo.hpp
+++ DayZ_Epoch_11.Chernarus/dayz_code/configs/Category - Namalsk/sniperRifleAmmo.hpp
@@ -6,8 +6,8 @@ class Category_624 {
 	};
 	class 5Rnd_762x51_M24 {
 		type = "trade_items";
-		buy[] = {100,"worth"};
-		sell[] = {50,"worth"};
+		buy[] = {1000,"worth"};
+		sell[] = {500,"worth"};
 	};
 	class 20Rnd_762x51_DMR {
 		type = "trade_items";
@@ -16,7 +16,7 @@ class Category_624 {
 	};
 	class 10Rnd_762x54_SVD {
 		type = "trade_items";
-		buy[] = {200,"worth"};
-		sell[] = {100,"worth"};
+		buy[] = {2000,"worth"};
+		sell[] = {1000,"worth"};
 	};
 };
